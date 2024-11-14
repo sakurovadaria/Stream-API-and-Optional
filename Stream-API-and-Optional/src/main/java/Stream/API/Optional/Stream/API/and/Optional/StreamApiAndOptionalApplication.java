95ecf264efd636e246e05b125b0cd635b1ed13df
pc <sakurovadaria@gmail.com>
2024-11-15 00:04:29 +0500

    First commit

---
+++ Stream-API-and-Optional/src/main/java/Stream/API/Optional/Stream/API/and/Optional/StreamApiAndOptionalApplication.java
@@ -0,0 +1,13 @@
+package Stream.API.Optional.Stream.API.and.Optional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApiAndOptionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamApiAndOptionalApplication.class, args);
+	}
+
+}
